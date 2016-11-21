ad2347b34e38e6da771b363d75599b46346455a8
Charlie Jonas <charlie@charliejonas.co.uk>
2016-11-21 21:50:43 +0000

    #define the values

--- Practical 3/ln2.cpp
+++ Practical 3/ln2.cpp
@@ -3,6 +3,10 @@
 #include <cstdlib>
 #include <cstdio>
 
+#define NUMBER_OF_ITERATIONS 10
+#define INTEGRATION_LIMIT_LOWER 1.0
+#define INTEGRATION_LIMIT_UPPER 2.0
+
 using namespace std;
 
 float f(float x) {
@@ -11,26 +15,23 @@ float f(float x) {
 
 int main(int argc, char* argv[]) {
 
-	int N=10;
-	// limits of integration
-	float a = 1.0, b = 2.0;
-	
+	int N = NUMBER_OF_ITERATIONS;
 	if(argc>1) {
 		// put the first command-line argument in N
 		sscanf( argv[1], "%d", &N );
 	}
 
 	// define the constants we will be using
-	float result = 0, scaleFactor = (b - a) / N;
+	float result = 0, scaleFactor = (INTEGRATION_LIMIT_UPPER - INTEGRATION_LIMIT_LOWER) / N;
 
 	// boundary conditions
-	result += scaleFactor * f(a) / 2;
-	result += scaleFactor * f(b) / 2;
+	result += scaleFactor * f(INTEGRATION_LIMIT_LOWER) / 2;
+	result += scaleFactor * f(INTEGRATION_LIMIT_UPPER) / 2;
 
 	// perform the iteration
 	for (int k = 1; k<=N-1; k++) {
 		// calculate the new result
-		result = result + scaleFactor * f(a + k * (b - a) / N);
+		result = result + scaleFactor * f(INTEGRATION_LIMIT_LOWER + k * (INTEGRATION_LIMIT_UPPER - INTEGRATION_LIMIT_LOWER) / N);
 		// print it
 		cout << "Iteration No. " << k << "\t\t" << result << endl;
 	}
